2fc40ec85d9800f4bd4015620fc520188a094f04
miaozhiwei <36184670+miaozhiwei@users.noreply.github.com>
2018-11-27 21:24:19 +0800

    Update main_check_alive.py
    增加业务逻辑，连续失败3次后才发邮件。避免因网络波动出现偶发性连接失败误发邮件。

--- main_check_alive.py
+++ main_check_alive.py
@@ -7,25 +7,33 @@ from send_email_alive import send_email
 # MQ 地址
 mq_ip = 'localhost'
 # MQ 端口
-mq_port = 15671
+mq_port = 5672
 ##################################################
 # 检查频率（秒）
 check_alive_loop = 60
+# 连续失败次数
+check_alive_times = 0
 ##################################################
 
 
 # 检查MQ队列是否连接
 def check_alive(ip, port):
-    print('Check Loop:', datetime.datetime.now())
+    global check_alive_times
+    print('Check Alive Loop:', datetime.datetime.now())
     sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sk.settimeout(1)
+    sk.settimeout(3)
     try:
         # 连接服务器
-        sk.connect((ip, port))
+        s = sk.connect((ip, port))
+        # 如果连接成功，计数器清零。如果连接失败，不会执行。
+        check_alive_times = 0
     except:
-        print('连接RabbitMQ服务器失败，请检查网络。')
-        # 发邮件,发邮件频率由调用方法自己控制。
-        send_email()
+        check_alive_times = check_alive_times + 1
+        print('连接RabbitMQ服务器失败 %d 次' % check_alive_times)
+        if check_alive_times >= 3:
+            # 发邮件,发邮件频率由调用方法自己控制。
+            send_email()
+            check_alive_times = 0
     sk.close()
     # 因为定时器构造后只执行1次，必须循环调用。
     global check_alive_timer
